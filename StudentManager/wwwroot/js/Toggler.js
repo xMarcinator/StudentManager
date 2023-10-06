44337803b44ff0bf8fddc5d5084be69425588a30
Marc Jensen <marcjensenvirklund@gmail.com>
2023-10-06 18:05:37 +0200

    more progress again

--- StudentManager/wwwroot/js/Toggler.js
+++ StudentManager/wwwroot/js/Toggler.js
@@ -1,30 +1,47 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿// Please see documentation at https://docs.mcd icrosoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets
-let toggler = document.getElementById("ThemeToggler")
-let root = document.getElementsByTagName("html")[0];
+let isDark = getThemeStorage();
+let root = document.getElementsByTagName("html")[0]
+/** @type {HTMLHtmlElement} */
+let toggle
+//set theme on start to prevent theme flickering
+setBootstrapTheme(isDark)
 
-function setTheme(isDark){
-    //hello
-    console.log("setting theme")
-    if (isDark) {
-        console.log("setting dark")
-        toggler.classList.remove("fa-sun");
-        toggler.classList.add("fa-moon");
-        root.setAttribute("data-bs-theme", "dark");
-    } else {
-        toggler.classList.remove("fa-moon");
-        toggler.classList.add("fa-sun");
-        root.setAttribute("data-bs-theme", "light");
-    }
+//wait for the dom to be loaded
+window.addEventListener("load", ()=>{
+    //set toggle
+    setToggle(isDark);
+    toggle = document.getElementById("ThemeToggler");
+    
+    document.getElementById("ThemeToggler")
+        .addEventListener("click", function (_) {
+        isDark = !isDark;
+        setBootstrapTheme(isDark);
+        setToggle(isDark);
+        setThemeStorage(isDark);
+    });
+})
+
+function getThemeStorage(){
+    return JSON.parse(localStorage.getItem("theme")) === true;
+}
+function setThemeStorage(isDark){
+    return localStorage.setItem("theme",JSON.stringify(isDark));
 }
 
-window.addEventListener("load", (event) => {
+function setBootstrapTheme(isDark) {
+    if (typeof (isDark) !== typeof true)
+        isDark = true;
 
-});
+    document.getElementsByTagName("html")[0].dataset["bsTheme"] = (isDark ? "dark" : "light");
+}
 
+const iconClass = ["fa-moon", "fa-sun"];
+function setToggle(isDark) {
+    if (typeof (isDark) !== typeof true)
+        isDark = true;
 
-// Write your JavaScript code.
-toggler.addEventListener("click", function (e) {
-    setTheme(root.getAttribute("data-bs-theme") === "light")
-});
+    document.getElementById("ThemeToggler")?.classList.replace(iconClass[+isDark], iconClass[+(!isDark)]);
+}
+
 
